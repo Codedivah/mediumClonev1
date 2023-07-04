2eccc0732d733b53dd1c435fa95e09e4c88a58f0
haryobamy <haryobamy.badmus@gmail.com>
2023-07-04 22:48:02 +0100

    update

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb+srv://badmus:olawole27@cluster1.ondgqvx.mongodb.net/";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
