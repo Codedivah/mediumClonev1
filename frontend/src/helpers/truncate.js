1770d02677d9f5fd0e35adcb60a72b9736852471
FATIMAH BADMUS <95576634+Codedivah@users.noreply.github.com>
2023-07-07 00:11:55 +0100

    Update truncate.js

--- frontend/src/helpers/truncate.js
+++ frontend/src/helpers/truncate.js
@@ -1,3 +1,9 @@
+import axios from "axios"
+
 export function truncate(str, n) {
   return str?.length > n ? str.substr(0, n - 1) + "..." : str;
 }
+
+
+
+axios.defaults.baseURL = 'https://chatter-backend-ig7hnkr54-codedivah.vercel.app/';
